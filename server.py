7ce28fa02d46a58d9e179cb603b55cf96b86d628
led <laraserie@gmail.com>
2025-07-13 11:01:33 +0200

    first

--- server.py
+++ server.py
@@ -8,3 +8,5 @@ handler = http.server.SimpleHTTPRequestHandler
 with socketserver.TCPServer(("", PORT), handler) as httpd:
     print(f"Serving on http://localhost:{PORT}")
     httpd.serve_forever()
+
+# hun
